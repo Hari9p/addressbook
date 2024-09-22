ddf413ef17b6d42de27f7b04d46cd5b2baf46d96
Haribabu <hari9p@gmail.com>
2024-09-22 17:49:23 +0530

    ssh agent

---
+++ server-config.sh
@@ -0,0 +1,15 @@
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install git -y
+sudo yum install maven -y
+if [ -d "addressbook" ]
+ then 
+ echo "repo is closed and exists"
+ cd /home/ec2-user/addressbook
+ git pull origin jenkins-1
+
+ else
+
+git clone https://github.com/Hari9p/addressbook.git
+fi
+cd /home/ec2-user/addressbook
+mvn package
