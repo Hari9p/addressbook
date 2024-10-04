e510839a0604467e3e2be21a46fb4fd9900f8619
Haribabu <hari9p@gmail.com>
2024-10-04 17:32:18 +0530

    dockerfile

--- server-config.sh
+++ server-config.sh
@@ -1,15 +1,19 @@
-sudo yum install java-1.8.0-openjdk-devel -y
+# sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
-sudo yum install maven -y
+# sudo yum install maven -y
+sudo yum install docker -y
+sudo systemctl start docker
 if [ -d "addressbook" ]
  then 
  echo "repo is closed and exists"
  cd /home/ec2-user/addressbook
- git pull origin jenkins-1
+ git pull origin cicd-docker-1
 
  else
 
 git clone https://github.com/Hari9p/addressbook.git
-fi
 cd /home/ec2-user/addressbook
-mvn package
+git pull origin cicd-docker-1
+fi
+sudo docker build -t $1:$2 /home/ec2-user/addressbook
+
